d0f43718e6a7ba03c59814402921c2abe3d58da6
el-keiyya <keiyya@outlook.com>
2025-11-13 11:15:16 +0300

    Fix base

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const repoBase =
+  process.env.DEPLOY_TARGET === "GH_PAGES" ? "/qr-code-component/" : "/";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/qr-code-component/",
+  base: repoBase,
 });
