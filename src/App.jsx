cee4ec0e8587aadc45dcda8faeaebe294b6914c6
el-keiyya <keiyya@outlook.com>
2025-11-13 10:56:40 +0300

    Add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
+import QRCode from "../src/images/image-qr-code.png";
+
 export default function App() {
   return (
     <main className="bg-white p-4 flex flex-col space-y-3 text-center w-[320px] rounded-lg">
-      <img
-        src="../src/images/image-qr-code.png"
-        alt="QR Code"
-        className="w-full rounded-lg"
-      />
+      <img src={QRCode} alt="QR Code" className="w-full rounded-lg" />
       <div className="space-y-3 p-2">
         <h1 className="font-bold text-xl text-slate-900">
           Improve your front-end skills by building projects
